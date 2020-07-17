691da9f09459639c06f2c69fdc519f1505bfc098
Suso <susopc1981@hotmail.com>
2020-07-17 10:08:54 +0200

    Update state App with Search Input

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Search from "./components/Search";
 // HACER UNA LLAMADA A UN API PARA QUE NOS MUESTRE LOS TRENDING
 // UN BUSCADOR QUE NOS DEVUELVA LOS GIPH QUE BUSQUEMOS POR REFERENCIA
 function App() {
-	const [searchInput, setSearchInput] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
-	return (
-		<div className="App">
-			<Search />
-		</div>
-	);
+  const handleClick = (text) => {
+    setSearchInput(text);
+    //Llamada API
+  };
+
+  return (
+    <div className="App">
+      <Search handleClick={handleClick} />
+    </div>
+  );
 }
 
 export default App;
