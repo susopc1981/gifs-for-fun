10ba5c479346d79fddc52f1f0dc3d852b2a407be
Pablo <pabloneiro@hotmail.com>
2020-07-20 12:23:14 +0200

    Pablo update

--- src/api/getApi.js
+++ src/api/getApi.js
@@ -1,16 +1,28 @@
 import { baseUrl, apiKey } from "./constants";
 
 const getApi = async (search) => {
+  const data = {
+    arrayUrls: null,
+    error: "",
+  };
+
   let url = baseUrl + "trending?api_key=" + apiKey;
   if (search !== "") {
     url = baseUrl + "search?api_key=" + apiKey + "&q=" + search;
   }
   const result = await fetch(url);
+  console.log(result.status);
   const resultJson = await result.json();
-  const arrayUrls = resultJson.data.map(
-    (value) => value.images.preview_gif.url
-  );
-  return arrayUrls;
+  console.log(resultJson);
+  if (result.status === 200) {
+    const arrayUrls = resultJson.data.map(
+      (value) => value.images.preview_gif.url
+    );
+    data.arrayUrls = arrayUrls;
+  } else if (result.status === 403) {
+    data.error = "Error 403";
+  }
+  return data;
 };
 
 export { getApi };
